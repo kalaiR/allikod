2006ad9a04e45e75bbd0a1c44695d83d008351a0
selva <selva@etekchnoservices.com>
2017-07-14 13:01:44 +0530

    remove reset button in search page

--- application/views/search.php
+++ application/views/search.php
@@ -175,7 +175,6 @@
                                                 </div>
                                                 <div class="search_btn">
                                                     <button type="submit" value="LogIn" class="btn form-control login_btn">Search</button>
-                                                    <button type="reset" value="LogIn" class="btn form-control login_btn">Reset</button>
                                                 </div>
                         	 	 			 </form>
                         	 	 		</div>
@@ -376,10 +375,9 @@
                                                             </div>
                                                         </div>        
                                                 </div>
-                                                <div class="search_btn">
-                                                    <button type="submit" value="LogIn" class="btn form-control login_btn">Search</button>
-                                                    <button type="reset" value="LogIn" class="btn form-control login_btn">Reset</button>
-                                                </div>
+                                                    <div class="search_btn">
+                                                        <button type="submit" value="LogIn" class="btn form-control login_btn">Search</button>
+                                                    </div>
                                              </form>
                                         </div>
                                     </div>
