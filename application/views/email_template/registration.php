5cf9f5b34a685aa04554a6009105e12ece1fb6f8
Ravichandran <ravi@etekchnoservices.com>
2017-09-13 15:11:24 +0530

    Email template Editing

--- application/views/email_template/registration.php
+++ application/views/email_template/registration.php
@@ -18,7 +18,7 @@ $to_mailForSender ="valli.vallikodi@gmail.com";
 //$to_mailForSender  = "$customeremail,$vallikodiemail";
 //$to_mailForSender  = "rajeswari@atomicka.com,yogi.a89@gmail.com";
 $subject_mailForSender = 'Vallikodi Vanniar Matrimonial';
-// message --> -->
+// message  -->
 <!-- $message_mailForSender = ' -->
 
 <div style=" width:960px;height:auto;margin:0 auto;">
@@ -33,7 +33,7 @@ $subject_mailForSender = 'Vallikodi Vanniar Matrimonial';
                 <p1>Login Details</p1>
             </div>
             <div style="width: 200px; display: inline-block;margin-left: 150px; text-align: center; color: #fff;">
-                <p1>No : 12345678910</p1>
+                <p1>Vallikodi ID : 12345678910</p1>
             </div>
         </div>
         <div>
@@ -41,64 +41,89 @@ $subject_mailForSender = 'Vallikodi Vanniar Matrimonial';
                 <img src="images/banner.png">
             </div>
             <div style="color: #3c6a3c;margin-top: 30px; margin-left: 30px; font-size:25px; font-weight: bold;">
-                <p1>Dear : selva kumar<p1>
+                <p1>Dear Selva Kumar VM67450, </p1>
             </div>
             <div style="color:black;margin-top:5px; margin-left: 45px; ">
-                <p1>Welcome to Vallikodi Vanniar Matrimonial.Thanks for Registered with us.Our support team will contact you soon for Verification process.We will activate your account once finished our Verification process. (Net Banking payment will be avilable)<p1>
+                <p1><!-- Dear Selva Kumar VM67450, --> Your profile has been Activated. For further Details please check your mail. Thanks By Vallikodi Team.</p1>
             </div>
+            
             <div style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
-                <p1>Your Login Credentials :- <p1>
+                <p1>Your Login Credentials :- </p1>
             </div>
             <div style="color:black;margin-top:5px; margin-left: 45px;">
                 <div style="width: 200px; display: inline-block;">
-                    <p1>Vallikodi ID <p1>
+                    <p1>Vallikodi ID </p1>
                 </div>
                 <div style="width: 200px; display: inline-block;">
-                    <p1>: selva kumar<p1>
+                    <p1>: selva kumar</p1>
                 </div>
                 <div style="padding-top: 10px;">
                     <div style="width: 200px;display: inline-block;">
-                        <p1>User Name/Login ID<p1>
+                        <p1>User Name/Login ID</p1>
                     </div>
                     <div style="width: 200px; display: inline-block;">
-                        <p1>: selva kumar12345<p1>
+                        <p1>: selva kumar12345</p1>
                     </div>
                 </div> 
                 <div style="padding-top: 10px;">
                     <div style="width: 200px;display: inline-block;">
-                        <p1>Password<p1>
+                        <p1>Password</p1>
                     </div>
                     <div style="width: 200px; display: inline-block;">
-                        <p1>: selva kumar12345<p1>
+                        <p1>: selva kumar12345</p1>
+                    </div>
+                </div>
+            </div>
+            <div style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
+                <p1>Other Details :- </p1>
+            </div>
+            <div style="color:black;margin-top:5px; margin-left: 45px;">
+                <div style="width: 200px; display: inline-block;">
+                    <p1>Period in Months</p1>
+                </div>
+                <div style="width: 200px; display: inline-block;">
+                    <p1>: 3</p1>
+                </div>
+                <div style="padding-top: 10px;">
+                    <div style="width: 200px;display: inline-block;">
+                        <p1>Start Date</p1>
+                    </div>
+                    <div style="width: 200px; display: inline-block;">
+                        <p1>: 12-05-2017</p1>
+                    </div>
+                </div> 
+                <div style="padding-top: 10px;">
+                    <div style="width: 200px;display: inline-block;">
+                        <p1>End Date</p1>
+                    </div>
+                    <div style="width: 200px; display: inline-block;">
+                        <p1>: 12-09-2017</p1>
+                    </div>
+                </div>
+                <div style="padding-top: 10px;">
+                    <div style="width: 200px;display: inline-block;">
+                        <p1>Total no. Profiles</p1>
+                    </div>
+                    <div style="width: 200px; display: inline-block;">
+                        <p1>: 60</p1>
+                    </div>
+                </div>
+                <div style="padding-top: 10px;">
+                    <div style="width: 200px;display: inline-block;">
+                        <p1>Payment Status<p1>
+                    </div>
+                    <div style="width: 200px; display: inline-block;">
+                        <p1>: Active</p1>
                     </div>
                 </div>
             </div>            
         </div>
-        <div style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
-            <p1>Payment Details :-<p1>
-        </div>
-        <div style="color:black;margin-top:10px; margin-left: 45px; font-weight: bold; ">
-            <p1>1) Fee structure details in INDIA :-</p1>
-        </div>
-        <div style="color:black;padding-top:10px; margin-left: 70px;">
-            <p1>3 Months validity & display data with photo Rs.2500/- [60 Profiles can be viewed];<br>
-                6 Months validity & display data with photo Rs.4000/- [120 Profiles can be viewed];
-            </p1>
-        </div>
-        <div style="color:black;margin-top:10px; margin-left: 45px; font-weight: bold; ">
-            <p1>2) Fee structure details in Renewal fee:-</p1>
-        </div>
-        <div style="color:black;padding-top:10px; margin-left: 70px;">
-            <p1>3 Months validity & display data with photo Rs.2000/- [60 Profiles can be viewed];<br>
-                6 Months validity & display data with photo Rs.3500/- [120 Profiles can be viewed];
-            </p1>
-        </div>
-        <div style="color:black;padding-top:10px; margin-left: 45px;">
+        <div style="color:black;padding-top:40px; margin-left: 45px;">
             <p1>Thank you for being a Vallikodi Vanniar Matrimonial family member,</p1>
         </div>
         <div style="color:black;margin-top:45px; margin-left: 45px;">
             <p1>With Warm Regards,</p1>
-            <div style="color:black;padding-top:50px;font-weight: bold; ">
+            <div style="color:black;padding-top:35px;font-weight: bold; ">
                 <p1>VALLIKODI VANNIAR MATRIMONIAL TEAM.</p1>
             </div>
             <div>
