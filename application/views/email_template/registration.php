838df5b910fad2478932b926012904a0e1374e66
Banumathy M <banu@etekchnoservices.com>
2018-05-11 13:36:32 +0530

    email_template updated.

--- application/views/email_template/registration.php
+++ application/views/email_template/registration.php
@@ -21,75 +21,69 @@ $subject_mailForSender = 'Vallikodi Vanniar Matrimonial';
 // message --> 
 <!-- $message_mailForSender = ' -->
 
-<div style=" width:960px;height:auto;margin:0 auto;">
-        <div style=" background-color: #5c7500; margin-top: 50px;height: 50px;">
-            <p style="text-align: center;color:#fff; padding-top: 15px;">Vallikodi Vanniyar Martimonial.com</p>
-        </div>
-        <div style="background-color:#74872e;height: 30px;">
-            <div style=" width: 200px; padding-top:5px;padding-left: 5px; color: white;display: inline-block; ">
-                <p1>Date : <?php echo date("d-M-Y") ?></p1>
-            </div>
-            <div style="width: 200px; display: inline-block;margin-left: 150px; text-align: center; color: #fff;">
+<table style=" width:960px;height:auto;margin:0 auto; border="0"; cellpadding="0"; cellspacing="0">
+         <tr style=" background-color: #5c7500; margin-top: 50px;height: 50px;">
+            <td><p style="text-align: center;color:#fff; padding-top: 15px;">Vallikodi Vanniyar Martimonial.com</p></td>
+        </tr>
+        <tr style="background-color:#74872e;height: 30px;">
+            <td style=" width: 200px; padding-top:5px;padding-left: 5px; color: white;display: inline-block; ">
+                <p1>Date : data</p1>
+            </td>
+            <td style="width: 200px; display: inline-block;margin-left: 150px; text-align: center; color: #fff;">
                 <p1>Login Details</p1>
-            </div>
-            <div style="width: 200px; display: inline-block;margin-left: 150px; text-align: center; color: #fff;">
-                <p1>Vallikodi ID : <?php echo "VM".$user_id; ?></p1>
-            </div>
-        </div>
-        <div>
-            <div style="margin-left: 50px;">
-                <img src="<?php echo media_url(); ?>assets/img/logo1.png">
-            </div>
-            <div style="color: #3c6a3c;margin-top: 30px; margin-left: 30px; font-size:25px; font-weight: bold;">
-                <p1>Dear <?php echo $user_fname; ?><p1>
-            </div>
-            <div style="color:black;margin-top:5px; margin-left: 45px; ">
-                <!-- <p1>Welcome to Vallikodi Vanniar Matrimonial.Thanks for Registered with us.Our support team will contact you soon for Verification process.We will activate your account once finished our Verification process. (Net Banking payment will be avilable)<p1> -->
-                <?php if($reg_purpose == "quick_reg"){ ?>
-                    <p1>Welcome to Vallikodi Vanniar Matrimonial.Thanks for Registered with us. You have completed only quick registration process. Still you have to give more data's to complete your registration process with us. Once you finished all those through full registration page, Our support team will contact you soon for Verification process. We will activate your account once finished our Verification process. (Net Banking payment will be avilable)<p1>
-                <?php }else{ ?>
-                    <p1>Welcome to Vallikodi Vanniar Matrimonial.Thanks for Registered with us.Our support team will contact you soon for Verification process.We will activate your account once finished our Verification process. (Net Banking payment will be avilable)<p1>
-                <?php } ?>
-            </div>
+            </td>
+            <td style="width: 200px; display: inline-block;margin-left: 150px; text-align: center; color: #fff;">
+                <p1>Vallikodi ID : data</p1>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img style="margin:0 50px;" src="assets/img/logo1.png">
+            </td>
+        </tr>
+        <tr style="color: #3c6a3c;margin-top: 30px; margin-left: 30px; font-size:25px; font-weight: bold;">
+                <td><p style="margin:10 30px;" >Dear username<p></td>
+        </tr>
+            <tr style="color:black;margin-top:5px; margin-left: 45px; ">
+                    <!-- <p1>Welcome to Vallikodi Vanniar Matrimonial.Thanks for Registered with us.Our support team will contact you soon for Verification process.We will activate your account once finished our Verification process. (Net Banking payment will be avilable)<p1> -->
+               <tr>
+                    <td><p style="margin:0 30px; text-align: justify;" >Welcome to Vallikodi Vanniar Matrimonial.Thanks for Registered with us. You have completed only quick registration process. Still you have to give more data's to complete your registration process with us. Once you finished all those through full registration page, Our support team will contact you soon for Verification process. We will activate your account once finished our Verification process. (Net Banking payment will be avilable)<p></td>
+                </tr>      
+            </tr>
             
-            <div style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
-                <p1>Your Login Credentials :- </p1>
-            </div>
-            <div style="color:black;margin-top:5px; margin-left: 45px;">
-                <div style="padding-top: 10px;">
-                    <div style="width: 200px; display: inline-block;">
-                        <p1>Vallikodi ID <p1>
-                    </div>
-                    <div style="width: 200px; display: inline-block;">
-                        <p1>: <?php echo "VM".$user_id; ?><p1>
-                    </div>
-                </div> 
-                <div style="padding-top: 10px;">
-                    <div style="width: 200px;display: inline-block;">
-                        <p1>User Name/Login ID</p1>
-                    </div>
-                    <div style="width: 200px; display: inline-block;">
-                        <p1>: <?php echo $user_email; ?><p1>
-                    </div>
-                </div> 
-                <div style="padding-top: 10px;">
-                    <div style="width: 200px;display: inline-block;">
-                        <p1>Password</p1>
-                    </div>
-                    <div style="width: 200px; display: inline-block;">
-                        <p1>: <?php echo $user_pwd; ?><p1>
-                    </div>
-                </div>
-            </div>            
-        </div>
-        <div style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
-            <p1>Payment Details :-<p1>
-        </div>
-        <div style="color:black;padding-top:10px; margin-left: 70px;">
-            <p1> Please check the payment details in below link: </br>
-            <a href="<?php echo base_url()."payment" ?>"><?php echo base_url()."payment" ?></a>
-            </p1>
-        </div>
+            <tr style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
+                <td><p style="margin:10px 30px; padding-bottom:15px;">Suggested Profile :- </p></td>
+            </tr>
+            <tr style="color:black">
+                <tr style="color: rgba(0,0,0,); text-align:center;">
+                    <td style="width: 200px; display: inline-block; /*background-color:rgba(192,192,192,0.2);*/ padding:15px; margin-right:15px; border-radius: 25px; border: 1px solid rgba(192,192,192,0.5);">
+                        <img style="max-width: 100%;" src="assets/img/no_image.jpg">
+                        <p><a href="#" style="text-decoration: none; color:#000;font-size: 12px;">Id:123</a></p>
+                        <p style="font-size: 11px;"><span style="padding:0 5px">25 yrs,</span><span style="padding:0 5px">BE,</span><span style="padding:0 5px">Star</span></p>
+                    </td>
+                    <td style="width: 200px; display: inline-block;/*background-color:rgba(192,192,192,0.2);*/padding: 15px;margin-right:15px; border-radius: 25px;border: 1px solid rgba(192,192,192,0.5);">
+                       <img style="max-width: 100%;" src="assets/img/no_image.jpg">
+                        <p><a href="#" style="text-decoration: none; color:#000;font-size: 12px;">Id:123</a></p>
+                        <p style="font-size: 11px;"><span style="padding:0 5px">25 yrs,</span><span style="padding:0 5px">B Tech,</span><span style="padding:0 5px">Star</span></p>
+                    </td>
+                    <td style="width: 200px; display: inline-block;/*background-color:rgba(192,192,192,0.2);*/ padding:15px; border-radius: 25px;border: 1px solid rgba(192,192,192,0.5);">
+                       <img style="max-width: 100%;" src="assets/img/no_image.jpg">
+                       <p><a href="#" style="text-decoration: none; color:#000;font-size: 12px;">Id:123</a></p>
+                       <p style="font-size: 11px;"><span style="padding:0 5px">25 yrs,</span><span style="padding:0 5px">MCA,</span><span style="padding:0 5px">Star</span></p>
+                    </td>
+                </tr> 
+            </tr>           
+        <tr style="color:black;margin-top:5px; margin-left: 30px; font-weight: bold; ">
+            <td><p style="margin:10px 30px;">Payment Details :-<p></td>
+        </tr>
+        <tr style="color:black;padding-top:10px; margin-left: 70px;">
+            <tr>
+                <td><p style="margin:10px 30px;"> Please check the payment details in below link:</p></td>
+            </tr>
+            <tr>
+                <td><a style="margin:10px 30px; " href="#">link</a></td>
+            </tr>
+        </tr>
         <!-- <div style="color:black;margin-top:10px; margin-left: 45px; font-weight: bold; ">
             <p1>1) Fee structure details in INDIA :-</p1>
         </div> -->
@@ -106,21 +100,26 @@ $subject_mailForSender = 'Vallikodi Vanniar Matrimonial';
                 6 Months validity & display data with photo Rs.3500/- [120 Profiles can be viewed];
             </p1>
         </div> -->
-        <div style="color:black;padding-top:40px; margin-left: 45px;">
-            <p1>Thank you for being a Vallikodi Vanniar Matrimonial family member,</p1>
-        </div>
-        <div style="color:black;margin-top:45px; margin-left: 45px;">
-            <p1>With Warm Regards,</p1>
-            <div style="color:black;padding-top:35px;font-weight: bold; ">
-                <p1>VALLIKODI VANNIAR MATRIMONIAL TEAM.</p1>
-            </div>
-            <div>
-                <img src="<?php echo media_url(); ?>assets/img/fav.ico">
-            </div>
-        </div>
-        <div style="background-color: #5c7500;height: 1px;">
-        </div>
-    </div>
+        <tr style="color:black;padding-top:40px; margin-left: 45px;">
+            <td><p style="margin:10px 30px;">Thank you for being a Vallikodi Vanniar Matrimonial family member,</p1></td>
+        </tr>
+        <tr style="color:black;margin-top:45px; margin-left: 45px;">
+            <tr>
+                <td><p style="margin:0px 30px;">With Warm Regards,</p></td>
+            </tr>
+            <tr>
+                <td style="color:black;padding-top:35px;font-weight: bold; ">
+                    <p style="margin:0px 30px;">VALLIKODI VANNIAR MATRIMONIAL TEAM.</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img style="margin:0px 30px;"src="img/fav.ico">
+                </td>
+            </tr>
+            <tr> <td style="margin:0px 30px;background-color: #5c7500;height: 1px;"></td>
+            </tr>
+    </table>
 <!-- '; -->
 
 <!-- // To send HTML mail, the Content-type header must be set
